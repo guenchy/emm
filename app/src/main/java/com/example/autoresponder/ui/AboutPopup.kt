975c0f84a130a3dbe2eecfacb0ed6523468d3ee4
jean emmanuel thelusma <84931386+guenchy@users.noreply.github.com>
2024-02-14 17:19:03 -0300

    Update AboutPopup.kt

--- app/src/main/java/com/example/autoresponder/ui/AboutPopup.kt
+++ app/src/main/java/com/example/autoresponder/ui/AboutPopup.kt
@@ -22,22 +22,7 @@ class AboutPopup : AppCompatActivity() {
         setContentView(R.layout.about_popup)
         close_dialog_button.setOnClickListener { finish() }
 
-        share_app.setOnClickListener {
-            val sendIntent = Intent()
-            sendIntent.action = Intent.ACTION_SEND
-            sendIntent.putExtra(Intent.EXTRA_TEXT,
-                    "Hey check out my app at: https://play.google.com/store/apps/details?id=" + BuildConfig.APPLICATION_ID)
-            sendIntent.type = "text/plain"
-            startActivity(sendIntent)
-        }
-
-        github_link.setOnClickListener {
-            val openURL = Intent(android.content.Intent.ACTION_VIEW)
-            openURL.data = Uri.parse("https://github.com/suyashm002/watsappp_bot/")
-            startActivity(openURL)
-        }
-
-    }
+     
 
 
 
