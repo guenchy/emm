e65f500a1ec38e40a21fc5506f1653f4b8a9cecf
guenchy <nickithelusma@gmail.com>
2024-02-14 16:00:57 -0300

    carga inicial del projecto android

---
+++ app/src/main/java/com/example/autoresponder/ui/SplashActivity.kt
@@ -0,0 +1,28 @@
+package com.example.autoresponder.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.example.autoresponder.R
+
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed(Runnable {
+            startActivity(Intent(this@SplashActivity, CustomizeMessageActivity::class.java))
+            finish()
+        }, 4000)
+
+
+
+    }
+
+
+
+}
